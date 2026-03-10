a7a3aa735a544f15353f7a975adb64aab6c4ea82
123321zZzZ <12345612344321zz12@gmail.com>
2026-03-10 21:27:56 +0400

    2
    Add 3 NIST tests: Frequency, Runs, LongestRun

---
+++ lab_2/constants.py
@@ -0,0 +1,14 @@
+# Названия файлов с последовательностями
+SEQ_FILES = [
+    "sequences/C++.txt",
+    "sequences/Java.txt",
+    "sequences/C.txt",
+]
+
+# Параметры тестов
+BLOCK_SIZE = 8
+NUM_BLOCKS = 16
+SEQUENCE_LENGTH = 128
+
+# Вероятности для longest run теста
+PROBS = [0.2135, 0.3641, 0.2305, 0.1919]
